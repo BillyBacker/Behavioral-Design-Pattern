a43980ba787d1af4413a0dd87a088ddcad4706ba
Weelapat <38096723+BillyBacker@users.noreply.github.com>
2022-11-08 12:19:27 +0700

    new commit

---
+++ Sort/SortStrategy.java
@@ -0,0 +1,10 @@
+package Sort;
+
+// DO NOT MODIFY ANYTHING BELOW THIS LINE!!
+import java.util.List;
+
+import Person.Person;
+
+public interface SortStrategy {
+    void customSort(List<Person> people);
+}
